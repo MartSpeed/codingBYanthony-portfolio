3521ea39231cdccf8529aa3e454d45577325a0b4
John Anthony <jmanthony.fullsail@gmail.com>
2023-03-25 06:24:15 -0500

    FEAT: added footer placeholder text

--- src/components/Footer/Footer.component.js
+++ src/components/Footer/Footer.component.js
@@ -1,10 +1,21 @@
 import '../../App.css'
 import React from 'react'
 
+/**
+ * - Twitch
+- GitHub
+- LinkedIn
+ * 
+ */
+
 export default function Footer(){
     return(
         <div>
-            FOOTER
+            <ul>
+                <li>Github</li>
+                <li>Twitch</li>
+                <li>LinkedIn</li>
+            </ul>
         </div>
     )
 }
